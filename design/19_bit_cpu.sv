fd687a28fc9f9da4063517a2dc2004126050e706
Kartik Sahajpal <kartik.sahajpal.ug20@nsut.ac.in>
2025-03-17 08:31:54 +0530

    Updated interfaces in 19_bit_cpu.sv

--- design/19_bit_cpu.sv
+++ design/19_bit_cpu.sv
@@ -22,24 +22,20 @@ module CPU (
     ProgramCounter pc (
         .clk(clk),
         .reset(reset),
-        .load(cpu_ctrl_if.LOAD_REG),
-        .inc(cpu_ctrl_if.INC_PC),
-        .in_address(mem_addr),
-        .out_address(pc_out)
+        .ctrl_bus_if(cpu_ctrl_if),
+        .address_bus_if(cpu_addr_if)
     );
 
     // Instruction Memory
     InstructionMemory imem (
         .clk(clk),
-        .addr(pc_out),
-        .data_out(instruction)
+        .addr_bus_if(cpu_addr_if)
     );
 
     // Instruction Register
     InstructionRegister ir (
         .clk(clk),
-        .reset(reset),
-        .load(cpu_ctrl_if.ENABLE),
+        .ctrl_bus_if(cpu_ctrl_if),
         .instr_in(instruction),
         .opcode(opcode),
         .reg_dst(reg_dst),
@@ -52,48 +48,35 @@ module CPU (
     // Register File
     RegisterFile regfile (
         .clk(clk),
-        .reset(reset),
-        .load(cpu_ctrl_if.LOAD_REG),
+        .ctrl_bus_if(cpu_ctrl_if),
         .src1(reg_src1),
         .src2(reg_src2),
         .dst(reg_dst),
         .data_in(alu_result),
-        .data_out1(cpu_data_if.data1),
-        .data_out2(cpu_data_if.data2)
+        .data_bus_if(cpu_data_if.data1)
     );
 
     // ALU
     ALU alu (
-        .opcode(opcode),
-        .mode(mode),
+        .ctrl_bus_if(cpu_ctrl_if),
+        .data_bus_if(cpu_data_if),
         .operand1(cpu_data_if.data1),
         .operand2(mode ? {12'b0, imm_value} : cpu_data_if.data2), // Select Register or Immediate
         .result(alu_result),
-        .flags(cpu_ctrl_if.FLAGS)
     );
 
     // Data Memory
     DataMemory dmem (
         .clk(clk),
-        .addr(cpu_addr_if.address),
-        .data_in(cpu_data_if.data1),
-        .data_out(mem_data),
-        .read(cpu_ctrl_if.RD_EN_DM),
-        .write(cpu_ctrl_if.WR_EN_DM)
+        .addr_bus_if(cpu_addr_if),
+        .data_bus_if(cpu_data_if),
+        .ctrl_bus_if(cpu_ctrl_if)
     );
 
     // Control Unit
     ControlUnit cu (
         .clk(clk),
-        .reset(reset),
-        .opcode(opcode),
-        .enable(cpu_ctrl_if.ENABLE),
-        .rd_en(cpu_ctrl_if.RD_EN),
-        .wr_en(cpu_ctrl_if.WR_EN),
-        .inc_pc(cpu_ctrl_if.INC_PC),
-        .load_reg(cpu_ctrl_if.LOAD_REG),
-        .mode(cpu_ctrl_if.MODE),
-        .flags(cpu_ctrl_if.FLAGS)
+        .ctrl_bus_if(cpu_ctrl_if)
     );
 
 endmodule
